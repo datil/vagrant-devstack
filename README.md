85956847cbcc2437c67f94b1e3798b8a6393d5c8
Eduardo Raad <eduardo.raad@datilmedia.com>
2014-06-26 08:59:18 -0500

    Runs provision script as non-root.

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 vagrant-devstack
 ================
 
-Devstack environment using Vagrant
+__vagrant-devstack__ is a [Vagrant](http://www.vagrantup.com/) environment 
+
+# Requirements
+* Windows, Linux or OS X
+* Install [Vagrant](http://www.vagrantup.com/)
+
+# Running the environment
+```
+$ git clone https://github.com/datilmedia/vagrant-devstack.git
+$ cd vagrant-devstack
+$ vagrant up
+```
+
+# To-do
+* Migrate shell provision script to Puppet
+* Devstack testing guide
--- bin/devstack.sh
+++ bin/devstack.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
-# http://devstack.org/guides/single-vm.html
 
+# Dependencies
 sudo apt-get -qqy update
 sudo apt-get install -qqy git
 cd /home/vagrant
+
+# Get latest Devstack source code
 git clone https://github.com/openstack-dev/devstack.git
+
+# Setup a basic configuration
 cd devstack
 echo '[[local|localrc]]' > local.conf
 echo ADMIN_PASSWORD=password >> local.conf
@@ -12,7 +16,13 @@ echo MYSQL_PASSWORD=password >> local.conf
 echo RABBIT_PASSWORD=password >> local.conf
 echo SERVICE_PASSWORD=password >> local.conf
 echo SERVICE_TOKEN=tokentoken >> local.conf
+
+# Run Devstack for the first time
 ./stack.sh
+
+# References
+# Devstack. Running a Cloud in a VM. http://devstack.org/guides/single-vm.html
+# Devstack. Configuration. http://devstack.org/configuration.html
 
 
 
